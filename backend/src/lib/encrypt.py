83a97dd0c800c1ecdcea350156540e8fa1eb825a
robert-min <robertmin522@gmail.com>
2023-07-09 20:10:19 +0900

    docs : Add EncryptManager docs

--- backend/src/lib/encrypt.py
+++ backend/src/lib/encrypt.py
@@ -1,3 +1,16 @@
+"""Encrypt&Decrypt  library
+
+EncryptManager:
+    - 유저 비밀번호 암호화를 위한 Encrypt Manager 입니다.
+    Functions:
+        - get_dek: 유저별 랜덤으로 대칭키를 생성합니다.
+        - encrypt_password: 유저의 비밀번호를 암호화합니다.
+        - get_user_auth: 유저의 비밀번호를 복호화합니다.
+
+Raises:
+    EncryptManagerError: EncryptManager 클래스에서 발생한 오류
+
+"""
 import base64
 from uuid import uuid4
 from Crypto.Cipher import AES
@@ -5,11 +18,21 @@ from Crypto.Util.Padding import pad, unpad
 
 
 class EncryptManager:
+    """
+    Encrypt Manager
+    """
     
     def __init__(self) -> None:
         self.Block_size = 16
         
     def get_dek(self) -> bytes:
+        """Get dek for encryption.
+        Return:
+            dek
+            
+        Raise:
+            Failed to get dek.
+        """
         try:
             return uuid4().bytes
         except Exception:
@@ -17,6 +40,17 @@ class EncryptManager:
         
         
     def encrypt_password(self, dek: bytes, origin_pw: str) -> bytes:
+        """Encrypt password.
+        Args:
+            dek: user dek
+            origin_pw: user password
+        
+        Return:
+            encrypt_pw
+            
+        Raise:
+            Failed to encrypt password.
+        """
         try:
             encoding_pw = origin_pw.encode()
             aes = AES.new(dek, AES.MODE_ECB)    # TODO : MODE_ECB => MODE_CBC 변경 필요!!
@@ -27,6 +61,17 @@ class EncryptManager:
             raise EncryptManagerError("Failed to encrypt password.")
     
     def decrypt_password(self, dek: bytes, encrypt_pw: bytes) -> str:
+        """Decrypt password.
+        Args:
+            dek: user dek
+            encrypt_pw: user encrypt password
+        
+        Return:
+            origin_pw
+            
+        Raise:
+            Failed to decrypt password.
+        """
         try:
             aes = AES.new(dek, AES.MODE_ECB)
             decrypt_pw = aes.decrypt(base64.b64decode(encrypt_pw))
